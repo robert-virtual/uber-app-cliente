14c6ba60c06aae57979fd5833cb1230a0d5f8944
robert-virtual <robertocastill63@gmail.com>
2022-03-29 15:26:21 -0600

    prisma

--- src/controllers/inicio.js
+++ src/controllers/inicio.js
@@ -1,9 +1,22 @@
-const users = Array.from(Array(50), (e, i) => ({
-  nombre: "Usuario " + i,
-  apellido: "Aplellido " + i,
-}));
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-exports.inicio = (req, res) => {
+exports.inicio = async (req, res) => {
+  const users = await prisma.clientes.findMany({
+    skip: 0,
+    take: 5,
+    select: {
+      nombre: true,
+      correo: true,
+      viajes: {
+        select: {
+          fecha: true,
+          destino: true,
+        },
+      },
+    },
+  });
+  console.log(users);
   res.render("inicio", {
     titulo: "Inicio",
     nombre: "Hola mundo handlebars",
