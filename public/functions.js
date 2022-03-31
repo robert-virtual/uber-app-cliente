e5f8be54c658e99da70aae40b65fb5e86f5852f4
robert-virtual <robertocastill63@gmail.com>
2022-03-31 16:06:50 -0600

    buscra lugares

--- public/functions.js
+++ public/functions.js
@@ -4,6 +4,8 @@ const d = document;
 const btnMiUbicacion = d.getElementById("btn-mi-ubicacion");
 const searchBar = d.getElementById("search-bar");
 const searchrResults = d.getElementById("search-results");
+let map;
+
 const mapStyles = [
   "mapbox://styles/mapbox/navigation-day-v1",
   "mapbox://styles/mapbox/streets-v11",
@@ -24,7 +26,7 @@ export function insertarMapa({ coords }) {
   ubicacion.lng = longitude;
 
   try {
-    const map = new Map({
+    map = new Map({
       container: "map", // container ID
       style: mapStyles[selectedStyle], // style URL
       center: [longitude, latitude], // starting position [lng, lat]
@@ -40,7 +42,7 @@ export function insertarMapa({ coords }) {
       .setPopup(popup)
       .addTo(map);
 
-    setbtnMiUbicacion(map);
+    setbtnMiUbicacion();
     setSearchBar();
   } catch (error) {
     removerLoader(error);
@@ -48,7 +50,7 @@ export function insertarMapa({ coords }) {
   removerLoader();
 }
 
-function setbtnMiUbicacion(map) {
+function setbtnMiUbicacion() {
   btnMiUbicacion.classList.remove("hidden");
   btnMiUbicacion.onclick = () => {
     console.log("btnMiUbicacion");
@@ -58,18 +60,71 @@ function setbtnMiUbicacion(map) {
     });
   };
 }
-
+const api_url = ``;
+//https://api.mapbox.com/geocoding/v5/mapbox.places/dsdsd.json?country=hn&proximity=ip&types=place%2Cpostcode%2Caddress&language=es&access_token=pk.eyJ1Ijoicm9iZXJ0Y2FzdDEyIiwiYSI6ImNremp5ZHdmNTF5OG8ybm9iZ2E1bGFhODAifQ.4gKPVXpglB0fZOIE7YEI1A
 function setSearchBar() {
   searchBar.classList.remove("hidden");
-  searchBar.onkeyup = () => {
-    const t = d.createElement("template");
+  let idTimeOut;
+  searchBar.onkeyup = ({ target }) => {
+    clearTimeout(idTimeOut);
+    idTimeOut = setTimeout(async () => {
+      if (!target.value) {
+        return;
+      }
+      let res = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${target.value}.json?country=hn&proximity=ip&types=place%2Cpostcode%2Caddress&language=es&access_token=pk.eyJ1Ijoicm9iZXJ0Y2FzdDEyIiwiYSI6ImNremp5ZHdmNTF5OG8ybm9iZ2E1bGFhODAifQ.4gKPVXpglB0fZOIE7YEI1A`
+      );
+      res = await res.json();
+      console.log(res);
+      searchrResults.innerHTML = "";
 
-    t.innerHTML = `
-      <div class="search-result">
-      Honduras tegucigalpa
-    </div>
-      `;
-    searchBar.append(t.content);
+      res.features.forEach(createResult);
+      searchrResults.classList.remove("hidden");
+      if (!res.features.length) {
+        createResult({
+          text: "No se encontrarn resultados",
+          place_name_es: target.value,
+        });
+      }
+    }, 500);
   };
-  searchrResults.classList.remove("hidden");
+}
+
+let markadorPrevio;
+function crearMarcador({ lat, lng }) {
+  if (markadorPrevio) {
+    markadorPrevio.remove();
+  }
+
+  markadorPrevio = new Marker({
+    color: "#00dbc5",
+  })
+    .setLngLat({ lat, lng })
+    .addTo(map);
+  map.flyTo({
+    center: { lat, lng },
+    zoom: 15,
+  });
+}
+
+function createResult({ text, place_name_es, center }) {
+  const t = d.createElement("template");
+  t.innerHTML = `
+          <div class="search-result cursor-pointer">
+          <p class="cursor-pointer">
+          ${text}
+          </p> 
+          <p>
+          ${place_name_es}
+          </p> 
+        </div>
+          `;
+  console.log("keyup");
+  if (center) {
+    t.content.querySelector("div").onclick = () => {
+      console.log(center);
+      crearMarcador({ lat: center[1], lng: center[0] });
+    };
+  }
+  searchrResults.append(t.content);
 }
