f80dca54c7c139ab7d8fd771e5c4e95f4a74f9d4
olmanmartinez99@gmail.com <102955184+Olmanmartinez@users.noreply.github.com>
2022-04-06 00:47:39 -0600

    Update

--- src/routes/ayuda.js
+++ src/routes/ayuda.js
@@ -2,7 +2,7 @@ const { getayuda, postayuda } = require("../controllers/ayuda");
 
 const router = require("express").Router();
 
-router.get("/", getrayuda);
+router.get("/", getayuda);
 
 router.post("/", postayuda);
 
