4657d5516ee755263108b3e1063c146009c54f67
robert-virtual <robertocastill63@gmail.com>
2022-04-06 14:07:07 -0600

    mapa comentarios

--- src/controllers/mapa.js
+++ src/controllers/mapa.js
@@ -1,8 +1,13 @@
 const { __prod__ } = require("../constantes");
+// __prod__ = valor boleano que indica
+// si estamos en un entorno de produccion o no
 
 exports.getMapa = (req, res) => {
   if (__prod__ && !req.session.userid) {
-    res.redirect("/login");
+    // si estamos en produccion y no hay una session iniciada
+    // redirigimos al usuario a el inicio ya que no tiene
+    // permiso para acceder a esta pagina
+    res.redirect("/");
     return;
   }
   res.render("mapa", {
