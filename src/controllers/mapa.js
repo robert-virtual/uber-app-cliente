b563e5d2e7e461391fa808c26511cf6d9d2e299e
robert-virtual <robertocastill63@gmail.com>
2022-04-06 14:09:36 -0600

    prod

--- src/controllers/mapa.js
+++ src/controllers/mapa.js
@@ -1,9 +1,5 @@
-const { __prod__ } = require("../constantes");
-// __prod__ = valor boleano que indica
-// si estamos en un entorno de produccion o no
-
 exports.getMapa = (req, res) => {
-  if (__prod__ && !req.session.userid) {
+  if (!req.session.userid) {
     // si estamos en produccion y no hay una session iniciada
     // redirigimos al usuario a el inicio ya que no tiene
     // permiso para acceder a esta pagina
